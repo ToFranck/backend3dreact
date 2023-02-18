235d0f81d8ea965e8a4435115bdad70df15f8088
ToFranck <73122097+ToFranck@users.noreply.github.com>
2023-02-18 20:07:46 +0100

    host

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
 
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+server.listen(3001, () => {
+    console.log('Server is listening on port 3001');
 });
