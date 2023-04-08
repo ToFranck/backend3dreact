9101ebd5344555eb2b26d9a44a6256965e0540d4
ToFranck <73122097+ToFranck@users.noreply.github.com>
2023-04-08 18:40:54 +0200

    add cd

--- models/artist.js
+++ models/artist.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const artistSchema = mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
+  cd : { type: String, required: true },
   image: { type: String, required: true },
 });
 
